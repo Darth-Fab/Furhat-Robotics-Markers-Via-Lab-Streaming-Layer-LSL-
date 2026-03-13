482a589e678b980742e3a8d65544d432ff879f13
Fabiola Diana <79273095+Darth-Fab@users.noreply.github.com>
2026-03-13 13:58:34 +0100

    Delete main/kotlin/furhatos/app/complimentbot directory

--- main/kotlin/furhatos/app/complimentbot/users.kt
+++
@@ -1,6 +0,0 @@
-package furhatos.app.complimentbot.flow
-
-import furhatos.flow.kotlin.NullSafeUserDataDelegate
-import furhatos.records.User
-
-var User.served by NullSafeUserDataDelegate { false }
